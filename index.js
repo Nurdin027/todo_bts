48aedd89b005cf5d863ad1ba6e8aae51c4074d5d
Nurdin027 <amarnurdin000@gmil.com>
2025-07-01 13:31:00 +0700

    Initial Commit

--- index.js
+++ index.js
@@ -5,13 +5,9 @@ const userRoutes = require("./src/routes/users");
 const checklistRoutes = require("./src/routes/checklist");
 const itemsRoutes = require("./src/routes/items");
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger/swagger.json")
-
 const app = express();
 
 app.use(express.json());
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/", userRoutes);
 app.use("/", checklistRoutes);
